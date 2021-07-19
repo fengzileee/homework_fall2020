9c386d7e874ff033c8be42cf04ad6cd0e3cf9268
Linfeng <linfengleee@gmail.com>
2021-07-19 17:26:46 +0800

    Migrate HW1 implementations

--- hw2/cs285/policies/MLP_policy.py
+++ hw2/cs285/policies/MLP_policy.py
@@ -86,8 +86,14 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
 
     # query the policy with observation(s) to get selected action(s)
     def get_action(self, obs: np.ndarray) -> np.ndarray:
-        # TODO: get this from hw1
-        return action
+        if len(obs.shape) > 1:
+            observation = obs
+        else:
+            observation = obs[None]
+
+        # Return the action that the policy prescribes
+        output = self.mean_net(ptu.from_numpy(observation))
+        return ptu.to_numpy(output)
 
     # update/train this policy
     def update(self, observations, actions, **kwargs):
@@ -98,9 +104,8 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
     # through it. For example, you can return a torch.FloatTensor. You can also
     # return more flexible objects, such as a
     # `torch.distributions.Distribution` object. It's up to you!
-    def forward(self, observation: torch.FloatTensor):
-        # TODO: get this from hw1
-        return action_distribution
+    def forward(self, observation: torch.FloatTensor) -> Any:
+        return self.mean_net(observation)
 
 
 #####################################################
